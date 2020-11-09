bd7c8d140be6fceac00e6eb8471ceafa1a63c4f9
Alex Bondor <alexandru.viorel.bondor@gmail.com>
2020-11-09 20:19:52 +0200

    Add redeploy.sh

--- redeploy.sh
+++ redeploy.sh
@@ -1,18 +1,11 @@
-#Prepare backend for deployment
-cd backend
-mvn package
-cd ..
+Prepare backend for deployment
 docker container rm -f backend
 docker image rm -f backend
-docker build -f backend.Dockerfile -t backend .
+docker build -f seaplol/backend.Dockerfile -t backend .
 
 #Prepare frontend for deployment
-cd frontend
-yarn install
-yarn run serve
-cd ..
 docker container rm -f frontend
 docker image rm -f frontend
-docker build -f frontend.Dockerfile -t frontend .
+docker build -f seaplol/frontend.Dockerfile -t frontend .
 
 docker-compose up
