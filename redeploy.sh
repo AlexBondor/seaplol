73dae94ee77fda2d902fe408afecdee2543df924
Alex Bondor <alexandru.viorel.bondor@gmail.com>
2020-11-09 20:24:18 +0200

    Fix dockerfiles and repo name

--- redeploy.sh
+++ redeploy.sh
@@ -1,11 +1,12 @@
+#Prepare backend for deployment
 Prepare backend for deployment
 docker container rm -f backend
 docker image rm -f backend
-docker build -f seaplol/backend.Dockerfile -t backend .
+docker build -f backend.Dockerfile -t backend .
 
 #Prepare frontend for deployment
 docker container rm -f frontend
 docker image rm -f frontend
-docker build -f seaplol/frontend.Dockerfile -t frontend .
+docker build -f frontend.Dockerfile -t frontend .
 
 docker-compose up
