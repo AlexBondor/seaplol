1843d3d1375fce81dc834dde2857cf48269595cd
Alex <alexx.cosma@gmail.com>
2020-11-10 22:35:15 +0200

    [ACC] - new FE link

--- frontend/src/api/http.js
+++ frontend/src/api/http.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "http://localhost:8081/";
+export const BASE_URL = "http://138.68.87.144:8081/";
 export const HTTP = axios.create({
   baseURL: BASE_URL
 });
