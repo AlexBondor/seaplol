716ae61ffe2cf0935dd71a2b42fa71fc0bcd709c
Alex <alexx.cosma@gmail.com>
2020-11-11 15:38:36 +0200

    - ...

--- backend/data-fetching/import-db-external.sh
+++ backend/data-fetching/import-db-external.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mongorestore --drop -d seaplol seaplol-db/seaplol/ -u root -p pR1R5vwx1i2D3kvJBcOOj --authenticationDatabase admin
+mongorestore -u root -p pR1R5vwx1i2D3kvJBcOOj --authenticationDatabase admin --drop -d seaplol seaplol-db/seaplol/ 
