fa2a35c77cbdf226ba4a17a36a5ff102549111c9
Alex <alexx.cosma@gmail.com>
2020-11-11 15:42:54 +0200

    - legat

--- backend/data-fetching/import-db-external.sh
+++ backend/data-fetching/import-db-external.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mongorestore -u root -p pR1R5vwx1i2D3kvJBcOOj --authenticationDatabase admin --drop -d seaplol seaplol-db/seaplol/ 
+mongorestore -u root -ppR1R5vwx1i2D3kvJBcOOj --authenticationDatabase admin --drop -d seaplol seaplol-db/seaplol/ 
